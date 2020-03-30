3b7d814d80fbeee7c180466fd2ab856ce81379c7
Evan Smith <professionalemail@zoho.com>
2020-03-30 21:18:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array.join
   elsif array.length == 2
-    return array.join(", ")
+    return array.join(" and ")
   elsif array.length > 2
     last_item = array.pop
     first_items = array.join(", ")
